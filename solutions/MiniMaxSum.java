4dfe9bcb0ae3913ce8b0281357850bb97d26a07d
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2024-07-15 10:12:06 +0530

    Create MiniMaxSum.java

---
+++ solutions/MiniMaxSum.java
@@ -0,0 +1,51 @@
+
+/*question link : https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+        long mins = 0, maxs = 0;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            mins += arr.get(i);
+            maxs += arr.get(i + 1);
+        }
+        System.out.println(mins + " " + maxs);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
